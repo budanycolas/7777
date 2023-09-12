343f2f2aed06210545609bab36453add921f084e
budanycolas <106027800+budanycolas@users.noreply.github.com>
2023-09-12 08:43:16 -0300

    S

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ function TocaSomPom () {
     document.querySelector('#som_tecla_pom').play();
 }
 
-const ListaDeSons =document.querySelectorAll('tecla');
+const ListaDeSons = document.querySelectorAll('tecla');
 
 let Contador = 0;
 
 while (Contador < 9) {
     ListaDeSons[Contador].onclick = TocaSomPom;
-    contador = contador =1;
+    Contador = Contador =1;
     console.log(Contador);
 
 
